11f7a91ccb29873261afa4e7ebb86b4411685146
MouadCHAAIRA <mouad.chaaira@usmba.ac.ma>
2021-09-17 00:36:45 +0100

    first commit

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $product=Product::latest()->get();
+        return response()->json(['products'=>$product]);
+    }
+
+    public function store(Request $req)
+    {
+        $product=new Product();
+        $product->title=$req->title;
+        $product->description=$req->description;
+        $product->photo=$req->photo;
+        $product->save();
+    }
+
+    public function update(Request $req, $id)
+    {
+        $product=Product::find($id);
+        $product->title=$req->title;
+        $product->description=$req->description;
+        $product->photo=$req->photo;
+        $product->save();
+    }
+
+    public function destroy($id)
+    {
+        Product::find($id)->delete();
+    }
+
+    public function show($id)
+    {
+        $product=Product::find($id);
+        return response()->json(['product'=>$product]);
+    }
+}
